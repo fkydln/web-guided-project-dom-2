c3c88ae31c091e7a2b8c8521db858b914a32a965
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-01 12:41:17 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ cancelButton.addEventListener("click", function (event) {
 // Add it as an event listener for 'keydown' events on document.
 document.addEventListener("keydown", function (event) {
   // if the event object contains the key 'escape', kill modal
-  if (event.key === "Escape") {
+  if (event.key === "Esc") {
     modal.classList.add("off");
   }
 });
